89a0ca11dd99e67296e878f7baa56091917a5f50
Glitch (insta--link) <none>
2021-05-14 13:45:37 +0000

    Instalink 2.0 - EXTRA's Intergrations

--- README.md
+++ README.md
@@ -1,16 +1,99 @@
-Welcome to InstaLink
-=================
+# Welcome to InstaLink!
 
-InstaLink is a simple Instagram Bio Link. Custom gardient backrounds, Links, Topics and more! 
+InstaLink is a simple Instagram Bio Link Generator. Instalink is the first (and only) bio link generator that has send-away messages
 
-Geting Started
-==============
+Send-away messages that are short mesages that your guests will see when they click on a link. An example is
 
-Download and upload the .zip file. You will only need the @template.html and style.css. All other files are not needed. Rename @tempate.html to your @your_username.html or your perfered url. To add a new url copy and paste line 37 - 40. Including the p tag! To add a new topic copy/paste line 40 Including the p tag. You should be good to go!
+`You are being teleported to` `Where ever they are being teleported to`
 
-2.0
-==================
-Version 2.0 is set to come out. 
+# Geting Started
 
-![image](https://user-images.githubusercontent.com/67153251/110797670-524d0400-8247-11eb-85f0-255f08cd7e03.png)
-2021 BenM
+Fist you will need to deploy Instalink to a host. Here are some quick start buttons:
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/Benjamin-del/instalink)
+
+[![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/import/github/benjamin-del/instalink)
+
+[![forks - instalink](https://img.shields.io/github/forks/benjamin-del/instalink?style=social)](https://github.com/benjamin-del/instalink)
+
+# Adding your links
+
+All of the Settings for Instalink are in link.json file 
+
+Here is an example link.json file
+
+```
+{
+  "user" : "benmmonster",
+  "user_url" : "benmmonster",
+  "media" : "https://instagram.com",
+  "symbol" : "@",
+  "media_name" : "Instagram",
+
+  
+  "msg" : "You are being teleported to",
+  
+  "title1": "Link1",
+  "link1": "https://google.com",
+
+  "title2": "Link2",
+  "link2": "https://google.com",
+
+  "title3": "Link3",
+  "link3": "https://google.com",
+
+  "title4": "Link4",
+  "link4": "https://google.com",
+  
+  "title5": "Link5",
+  "link5": "https://google.com"
+
+}
+```
+
+
+
+
+`user` Is your username. 
+
+
+`user_url` Is your username url ***DO NOT INCLUDE THE FULL LINK*** Only whatever comes after the domain (Ommit the first `/`) EX:
+
+`https://instagram.com/benmmonster` becomes `benmmonster`
+
+`media` is your social media name 
+
+`symbol` is the symbol from your name EX: `@benmmonster`
+
+`media_name` is the name of your primary social media
+
+`msg` is your send away message. The default is `You are being teleported to` ***YOU DO NOT NEED A SPACE AT THE END*** Instalink add's it in automaticly
+
+When a guest clicks on a link before they are redirected they will see `msg` + `link#`
+
+`title#` Is the title of the link 
+
+`link#` Is the destanation URL
+
+***NOTE***
+
+`title5` & `link5` Are turned off automaticly. You do not need to remove it from link.json file. See below on how to turn it off
+
+
+# Add a 5th link.
+
+Instalink has come with support for five links but it is turned off by default. To turn it on uncomment line 38 ***AND*** 45. This will show "Link 5"
+
+
+# Extras
+
+You may notice a folder called extras. This is where all the extra parts will go. (EX: Themes, 5th link, 6th link etc...) I will release extra's eventualy. All new versions of instalink come with 5.js (Dissabled). 
+
+I hope this is easy to understand. If you need more help create an issue in github and I will look in to it. Please see [Extras](../blob/master/EXTRAS.md)
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,5 +1,105 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var Link = JSON.parse(this.responseText);
+          document.getElementById("Link1").innerHTML = Link.title1;
+          document.getElementById("Link2").innerHTML = Link.title2;
+          document.getElementById("Link3").innerHTML = Link.title3;
+          document.getElementById("Link4").innerHTML = Link.title4;
+          document.getElementById("user").innerHTML = Link.symbol + Link.user;
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+
+        }
+      };
+      xmlhttp.open("GET", "link.json", true);
+      xmlhttp.send();
+
+      function click1() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var Link = JSON.parse(this.responseText);
+            document.getElementById("Link1").innerHTML =
+              Link.msg + "&nbsp;" + Link.link1;
+            console.log = Link.msg + "&nbsp;" + Link.link1;
+            window.location.href = Link.link1;
+          }
+        };
+        xmlhttp.open("GET", "link.json", true);
+        xmlhttp.send();
+      }
+
+      function click2() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var Link = JSON.parse(this.responseText);
+            document.getElementById("Link2").innerHTML =
+              Link.msg + "&nbsp;" + Link.link2;
+            window.location.href = Link.link2;
+            console.log = Link.msg + "&nbsp;" + Link.link;
+          }
+        };
+        xmlhttp.open("GET", "link.json", true);
+        xmlhttp.send();
+      }
+
+      function click3() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var Link = JSON.parse(this.responseText);
+            document.getElementById("Link3").innerHTML =
+              Link.msg + "&nbsp;" + Link.link3;
+            window.location.href = Link.link3;
+            console.log = Link.msg + "&nbsp;" + Link.link3;
+          }
+        };
+        xmlhttp.open("GET", "link.json", true);
+        xmlhttp.send();
+      }
+
+      function click4() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var Link = JSON.parse(this.responseText);
+            document.getElementById("Link4").innerHTML =
+              Link.msg + "&nbsp;" + Link.link4;
+            window.location.href = Link.link4;
+            console.log = Link.msg + "&nbsp;" + Link.link4;
+          }
+        };
+        xmlhttp.open("GET", "link.json", true);
+        xmlhttp.send();
+      }
+
+      function user() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var Link = JSON.parse(this.responseText);
+            document.getElementById("user").innerHTML =
+              Link.msg +
+              "&nbsp;" +
+              Link.symbol +
+              Link.user +
+              "&#39;s" +
+              "&nbsp;" +
+              Link.media_name +
+              "!";
+            console.log =
+              Link.msg +
+              "&nbsp;" +
+              Link.symbol +
+              Link.user +
+              "&#39;s" +
+              "&nbsp;" +
+              Link.media_name +
+              "!";
+            window.location.href = Link.media + "/" + Link.user_url;
+          }
+        };
+        xmlhttp.open("GET", "link.json", true);
+        xmlhttp.send();
+      }
