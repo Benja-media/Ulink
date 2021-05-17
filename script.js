66c91170da22132af24b18da9c8817c1348e6f0f
Benjamin-del <67153251+Benjamin-del@users.noreply.github.com>
2021-05-17 08:46:13 -0400

    Bug fix.
    Fix issue #9.

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-      var xmlhttp = new XMLHttpRequest();
+        window.onload = title();
+
+
+function title() {
+  
+var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var Link = JSON.parse(this.responseText);
+          document.title = Link.symbol + Link.user + " | Instalink";
+          document.getElementById("Atrubution").innerHTML =   "This page was created by " + Link.user + "! "+ Link.user + " used InstaLink to create this page Click here to get yours now!" ;
+
+        }
+      };
+      xmlhttp.open("GET", "link.json", true);
+      xmlhttp.send();
+}
+
+function Atrubution() {
+  window.location.href = "refer.html"
+}
+    
+/*DO NOT CHANGE ABOVE*/  
+
+
+
+var xmlhttp = new XMLHttpRequest();
       xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var Link = JSON.parse(this.responseText);
